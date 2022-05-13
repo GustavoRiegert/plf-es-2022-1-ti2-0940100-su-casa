9f49830b04f393e8fd38f3ed4925534063310160
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2022-05-12 21:30:16 -0300

    commit server

---
+++ src/SuCasa_server/src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @IsPublic()
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
