ec13862224b74f2dd2c3371ea232c47216c972d4
andresathler <andremoriya.sathler@gmail.com>
2022-05-16 12:44:18 -0300

    commit work

---
+++ stacks/SuCasa_web/SuCasa_react/src/components/paginaInicial.tsx
@@ -0,0 +1,10 @@
+import {HeaderComponent} from "./header/index-header.component";
+
+export function IndexComponents() {
+
+    return(
+        <header>
+            <HeaderComponent />
+        </header>
+    )
+}
