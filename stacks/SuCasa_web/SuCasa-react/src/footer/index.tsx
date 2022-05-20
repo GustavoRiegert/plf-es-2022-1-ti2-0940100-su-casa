a4efff9246f71bd1f953827689745a4a5f3a24e3
andresathler <andremoriya.sathler@gmail.com>
2022-05-20 18:24:05 -0300

    commit update web

--- stacks/SuCasa_web/SuCasa-react/src/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import './styles.scss';
-
-export function FooterInfo() {
-    return(
-        <div className="container-footer">
-            <p className="footer-info">André Avelar, Gustavo Riegerte, Nicólas, Yuri</p>
-        </div>
-    )
-}
